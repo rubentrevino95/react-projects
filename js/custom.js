d1058457c248e8df5a9e4b439653af1cd9260c1a
Ruben <rubentrevino95@gmail.com>
2020-09-16 22:07:51 -0500

    adding home interface

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,18 @@
 function Person(props) {
     return (
         <div className="person">
-            <h1>props.name</h1>
-            <p>Age: unknown</p>
+            <h1>{props.name}</h1>
+            <p>Age: {props.age}</p>
         </div>
     );
 }
+var app = (
+    <div>
+        <Person name="Max" age="25" />
+    </div>
+)
+
 // renders js 'person' function
 // function being rendered into targeted id
-ReactDOM.render(<person name="Max" age="25"/>, document.querySelector('#p1'))
+ReactDOM.render(app, document.querySelector('#app'))
+ReactDOM.render(<Person name="Ben" age="24"/>, document.querySelector('#p1'))
