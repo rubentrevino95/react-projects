078796d6a0e1b2ccf3bb353d507a17b1ebdaf5fa
Ruben <rubentrevino95@gmail.com>
2020-09-19 21:38:18 -0500

    starting three js proj

---
+++ js/three.js
@@ -0,0 +1,16 @@
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innrerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(2, 2, 2);
+
+var material = new THREE.MeshBasicMaterial( {color: 0x0000ff} );
+
+var cube = new THREE.Mesh(geometry, material);
+scene.add( cube );
