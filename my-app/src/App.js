2528c00e4b557f612fde4d4f7d5a953da9369042
Ruben <rubentrevino95@gmail.com>
2020-11-09 12:55:16 -0600

    finishing functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,7 +14,10 @@ function App() {
     if(evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
       .then(res => res.json())
-      .then(result => setWeather(result));
+      .then(result => {
+        setWeather(result)
+        setQuery('');
+      });
     }
   }
 
@@ -31,22 +34,26 @@ function App() {
   }
 
   return (
-    <div className="app">
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16)? 'app warm' : 'app') : 'app'}>
       <main>
         <div className='search-box'>
-          <input type='text' className='search-bar' placeholder='Search...'/>
+          <input type='text' className='search-bar' placeholder='Search...' 
+          onChange={e => setQuery(e.target.value)} 
+          value={query} 
+          onKeyPress={search}/>
         </div>
-        <div className='location-box'>
-          <div className='location'>
-          <div className='date'>{dateBuilder(new Date())}</div>
+        {(typeof weather.main !="undefined") ?(
+        <div>
+          <div className='location-box'>
+            <div className='location'>{weather.name}, {weather.sys.country}</div>
+            <div className='date'>{dateBuilder(new Date())}</div> 
+          </div>
+          <div className='weather-box'>
+            <div className='temp'>{Math.round(weather.main.temp)}°c</div>
+            <div className='weather'>{weather.weather[0].main}</div>
           </div>
         </div>
-        <div className='weather-box'>
-          <div className='temp'>
-            15 C
-          </div>
-          <div className='weather'>Sunny</div>
-        </div>
+      ) : ('')}
       </main>
     </div>
   );
