3878270f698b745c49208aaf91e78a37cfc13758
Ruben <rubentrevino95@gmail.com>
2020-11-09 12:35:36 -0600

    adding query/stying

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 const api = {
   key: "0d2cad07a4274e4d69b8a737ba5d6b9f",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if(evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => setWeather(result));
+    }
+  }
+
 
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
